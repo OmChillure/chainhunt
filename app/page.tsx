b1e72717128be72f1887dbe5d9b022b379778e1e
OmChillure <omchillure4@gmail.com>
2024-09-21 00:15:16 +0530

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import HeroSection from "@/components/homepage/hero-section";
 import { MarqueeDemo } from "@/components/homepage/marquee-page";
 import SideBySide from "@/components/homepage/side-by-side";
 import PageWrapper from "@/components/wrapper/page-wrapper";
-import config from "@/config";
 
 export default function Home() {
   return (
@@ -18,7 +17,6 @@ export default function Home() {
       <div className="max-w-[1200px] p-8 lg:mb-[5rem]">
         <BlogSample />
       </div>
-      {/* <div className="max-w-[1200px] p-8 mt-[2rem] lg:mt-[6rem] lg:mb-[5rem]"> */}
       <MarqueeDemo />
       <div className="flex justify-center items-center w-full my-[8rem]">
         <AccordionComponent />
